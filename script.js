e0305bbc769195d151f40d5f031c5ef62e16d1b8
Sean Yeo <seansqyeo@gmail.com>
2020-05-10 22:47:28 -0700

    create gameboard grid styles for html and css

---
+++ script.js
@@ -0,0 +1,15 @@
+let Gameboard = (function() {
+    let gameboard = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+        ];
+    
+})();
+
+let DisplayController = (function() {
+
+})();
+
+let PlayerFactory = 6;
+
