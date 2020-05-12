0df9e18a95906ab495a916bbae0cac255f5487d6
Sean Yeo <seansqyeo@gmail.com>
2020-05-11 22:35:06 -0700

    currently modifying gameBoard module

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 let Gameboard = (function() {
-    let gameboard = [
-        [null, null, null],
+    let _gameboard = [
+        ["x", "o", null],
         [null, null, null],
         [null, null, null]
         ];
+
+    // function that rerenders the board with playerMove
+    // also adds/removes event listeners upon a move
+    function renderBoard() {
+        for (let i = 0 ; i < _gameboard.length ; i++) {
+            for (let j = 0 ; j < _gameboard[i].length ; j++) {
+                console.log(_gameboard[i][j]);
+                if ()
+            }
+        }
+    }
+
+    function _refreshBoard() {
+        // reset gameboard values to null
+        Gameboard = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+        // add event listeners to all grid elements
+        let allPositions = document.querySelectorAll(".gameboardPosition");
+
+    }
+
+    function _eventListenerController() {
+        console.log("some more stuff");
+    }
+
+    return {
+        renderBoard,
+    };
     
 })();
 
-let DisplayController = (function() {
+Gameboard.renderBoard()
+console.log("liaurw");
 
-})();
-
-let PlayerFactory = 6;
-
+// game object would control whose turn it is x or o
